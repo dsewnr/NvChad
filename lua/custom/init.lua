93150abbf4b66c3bf273090dc058876c42f8af76
Peter BR Lu <peter_br_lu@trendmicro.com>
2021-10-04 15:35:58 +0800

    Add lspconfig keymap

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -21,10 +21,31 @@ local hooks = require "core.hooks"
 -- you can set one or many mappings
 -- example below:
 
--- hooks.add("setup_mappings", function(map)
---    map("n", "<leader>cc", "gg0vG$d", opt) -- example to delete the buffer
---    .... many more mappings ....
--- end)
+hooks.add("setup_mappings", function(map)
+   map("n", "<leader>cc", "gg0vG$d", opt) -- example to delete the buffer
+
+   -- lspconfig
+   local opts = { noremap = true, silent = true }
+   -- See `:help vim.lsp.*` for documentation on any of the below functions
+   map("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+   map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+   map("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+   map("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+   map("n", "gk", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+   map("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+   map("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+   map("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
+   map("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+   map("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+   map("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+   map("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+   map("n", "ge", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
+   map("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
+   map("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
+   map("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
+   map("n", "<space>fm", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+   map("v", "<space>ca", "<cmd>lua vim.lsp.buf.range_code_action()<CR>", opts)
+end)
 
 -- To add new plugins, use the "install_plugin" hook,
 -- NOTE: we heavily suggest using Packer's lazy loading (with the 'event' field)
